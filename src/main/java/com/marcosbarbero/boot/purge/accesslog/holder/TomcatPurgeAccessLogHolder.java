a3e30005b2ddc9c0f726804875af30ca38018dc2
Marcos Barbero <marcos.hgb@gmail.com>
2017-01-28 19:21:09 +0100

    Extract current log filename in a new method

--- src/main/java/com/marcosbarbero/boot/purge/accesslog/holder/TomcatPurgeAccessLogHolder.java
+++ src/main/java/com/marcosbarbero/boot/purge/accesslog/holder/TomcatPurgeAccessLogHolder.java
@@ -1,50 +1,56 @@
 package com.marcosbarbero.boot.purge.accesslog.holder;
 
+import com.marcosbarbero.boot.purge.accesslog.properties.PurgeProperties;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.valves.AccessLogValve;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+
 import java.io.File;
 import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.util.function.Supplier;
 
-import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import lombok.extern.slf4j.Slf4j;
 
 import static org.springframework.util.ReflectionUtils.findField;
 import static org.springframework.util.ReflectionUtils.makeAccessible;
-
-import com.marcosbarbero.boot.purge.accesslog.properties.PurgeProperties;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.Context;
-import org.apache.catalina.valves.AccessLogValve;
 
 /**
  * @author Matheus Góes
  */
 @Slf4j
 public class TomcatPurgeAccessLogHolder extends PurgeAccessLogHolder
-		implements TomcatContextCustomizer {
+        implements TomcatContextCustomizer {
 
-	public TomcatPurgeAccessLogHolder(final PurgeProperties purgeProperties,
-			final Path directory, final String prefix, final String suffix,
-			final AccessLogValve accessLogValve) {
-		super(purgeProperties, directory, prefix, suffix, () -> {
-			String fileName = null;
-			try {
-				final Field currentLogFileField = findField(accessLogValve.getClass(),
-						"currentLogFile", File.class);
-				makeAccessible(currentLogFileField);
-				final File currentLogFile = (File) currentLogFileField
-						.get(accessLogValve);
-				if (currentLogFile != null) {
-					fileName = currentLogFile.toPath().getFileName().toString();
-				}
-			}
-			catch (final Exception e) {
-				log.warn(e.getMessage(), e);
-			}
-			return fileName;
-		});
-	}
+    private static final String FIELD_NAME = "currentLogFile";
 
-	@Override
-	public void customize(final Context context) {
-		super.attachPurgeTask();
-	}
+    public TomcatPurgeAccessLogHolder(final PurgeProperties purgeProperties,
+                                      final Path directory, final String prefix, final String suffix,
+                                      final AccessLogValve accessLogValve) {
+        super(purgeProperties, directory, prefix, suffix, getCurrentLogFileName(accessLogValve));
+    }
+
+    private static Supplier<String> getCurrentLogFileName(final AccessLogValve accessLogValve) {
+        return () -> {
+            String fileName = null;
+            try {
+                final Field currentLogFileField = findField(accessLogValve.getClass(), FIELD_NAME, File.class);
+                makeAccessible(currentLogFileField);
+                final File currentLogFile = (File) currentLogFileField
+                        .get(accessLogValve);
+                if (currentLogFile != null) {
+                    fileName = currentLogFile.toPath().getFileName().toString();
+                }
+            } catch (final Exception e) {
+                log.warn(e.getMessage(), e);
+            }
+            return fileName;
+        };
+    }
+
+    @Override
+    public void customize(final Context context) {
+        super.attachPurgeTask();
+    }
 }
